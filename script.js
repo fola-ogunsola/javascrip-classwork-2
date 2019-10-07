067976e29cd620be5e8f5490e4e3ce929fbb7343
fola-ogunsola <54547114+fola-ogunsola@users.noreply.github.com>
2019-10-07 10:53:01 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+var username = prompt ("who is there");
+
+	if (username == "Admin") {
+		let password = prompt("Password");
+		if (userPassword == "The Master"){
+			alert ("welcome");
+		}
+		else if (userPassword ""){
+			alert ("cancelled"),
+		}
+		else alert ("wrong Password");
+	}
+	else if (username == ""){
+		alert("cancelled")
+	}
+	else alert("i dont know you");
+    
+
+
+var username = prompt ("who is there");
+switch (user) {
+	case "admin":
+	alert ("enter Password");
+	break;
+
+	case "null":
+	alert("cancelled");
+	break;
+
+	case "The Master":
+	alert("Welcome");
+	break;
+
+	default:
+	alert("wrong password");
+	break;
+
+	default:
+	alert("i dont know you");
+	break;
+}
